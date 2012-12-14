d1f1e591c7a3b53b19d037aa33c79c5c7606d411
Patrick Walton <pcwalton@mimiga.net>
2012-12-13 18:42:37 -0800

    Update for language changes

--- macos.rs
+++ macos.rs
@@ -1,22 +1,23 @@
-extern mod core_foundation;
-extern mod io_surface;
-extern mod opengles;
+pub extern mod core_foundation;
+pub extern mod io_surface;
+pub extern mod opengles;
 
-use mod opengles::cgl;
-use mod opengles::gl2;
+use mod platform::opengles::cgl;
+use mod platform::opengles::gl2;
 
 use core::cast::transmute;
 use core::ptr::{null, to_unsafe_ptr};
 use geom::size::Size2D;
-use io_surface::{IOSurface, kIOSurfaceBytesPerElement, kIOSurfaceBytesPerRow, kIOSurfaceHeight};
-use io_surface::{kIOSurfaceIsGlobal, kIOSurfaceWidth};
-use opengles::cgl::{CGLChoosePixelFormat, CGLContextObj, CGLCreateContext, CGLLockContext};
-use opengles::cgl::{CGLSetCurrentContext, CGLTexImageIOSurface2D, kCGLNoError, kCGLPFACompliant};
-use opengles::cgl::{kCGLPFADoubleBuffer};
-use opengles::gl2::{BGRA, CLAMP_TO_EDGE, COLOR_ATTACHMENT0, FRAMEBUFFER, FRAMEBUFFER_COMPLETE};
-use opengles::gl2::{GLenum, GLint, GLsizei, GLuint, LINEAR, NEAREST, RGBA, TEXTURE_MAG_FILTER};
-use opengles::gl2::{TEXTURE_MIN_FILTER, TEXTURE_RECTANGLE_ARB, TEXTURE_WRAP_S, TEXTURE_WRAP_T};
-use opengles::gl2::{UNSIGNED_INT_8_8_8_8_REV};
+use platform::io_surface::{IOSurface, kIOSurfaceBytesPerElement, kIOSurfaceBytesPerRow};
+use platform::io_surface::{kIOSurfaceHeight, kIOSurfaceIsGlobal, kIOSurfaceWidth};
+use platform::opengles::cgl::{CGLChoosePixelFormat, CGLContextObj, CGLCreateContext};
+use platform::opengles::cgl::{CGLLockContext, CGLSetCurrentContext, CGLTexImageIOSurface2D};
+use platform::opengles::cgl::{kCGLNoError, kCGLPFACompliant, kCGLPFADoubleBuffer};
+use platform::opengles::gl2::{BGRA, CLAMP_TO_EDGE, COLOR_ATTACHMENT0, FRAMEBUFFER};
+use platform::opengles::gl2::{FRAMEBUFFER_COMPLETE, GLenum, GLint, GLsizei, GLuint, LINEAR};
+use platform::opengles::gl2::{NEAREST, RGBA, TEXTURE_MAG_FILTER, TEXTURE_MIN_FILTER};
+use platform::opengles::gl2::{TEXTURE_RECTANGLE_ARB, TEXTURE_WRAP_S, TEXTURE_WRAP_T};
+use platform::opengles::gl2::{UNSIGNED_INT_8_8_8_8_REV};
 
 use base::ShareContext;
 
@@ -66,11 +67,10 @@ pub fn init_cgl() -> CGLContextObj {
 
 
 pub fn init_surface(+size: Size2D<int>) -> IOSurface {
-    use cf = core_foundation;
-    use cf::boolean::{CFBooleanRef, CFBoolean};
-    use cf::number::{CFNumberRef, CFNumber};
-    use number = cf::number::CFNumber::new;
-    use string = cf::string::CFString::wrap_extern;
+    use platform::core_foundation::boolean::{CFBooleanRef, CFBoolean};
+    use platform::core_foundation::number::{CFNumberRef, CFNumber};
+    use number = platform::core_foundation::number::CFNumber::new;
+    use string = platform::core_foundation::string::CFString::wrap_extern;
 
     // TODO: dictionary constructor should be less ridiculous.
     // Or, we could add bindings for mutable dictionaries.
@@ -89,7 +89,7 @@ pub fn init_surface(+size: Size2D<int>) -> IOSurface {
     let k_is_global = string(kIOSurfaceIsGlobal);
     let v_is_global = CFBoolean::true_value();
 
-    io_surface::new(&cf::dictionary::CFDictionary::new([
+    io_surface::new(&platform::core_foundation::dictionary::CFDictionary::new([
         (*k_width.borrow_ref(),          *v_width.borrow_type_ref()),
         (*k_height.borrow_ref(),         *v_height.borrow_type_ref()),
         (*k_bytes_per_row.borrow_ref(),  *v_bytes_per_row.borrow_type_ref()),
